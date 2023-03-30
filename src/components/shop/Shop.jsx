62d021ed0faa1a097a9e432bb4cdb4fc1ca2ff8b
Afroza-Khanom <afrozafra.aa@gmail.com>
2023-03-31 03:09:29 +0600

    icon added

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -4,6 +4,7 @@ import './Shop.css';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(()=>{
         fetch('products.json')
@@ -11,17 +12,25 @@ const Shop = () => {
         .then(data => setProducts(data))
     },[])
 
+    const handleAddtoCart = (product) =>{
+        // cart.push(product) notmally js e evbe cart e item add kra jeto
+        const newCart = [...cart, product];  //react e new array ready kore then add krte hoi
+        setCart(newCart)
+    }
+
     return (
         <div className='shop-container'>
             <div className='products-container'>
                 {
                     products.map(product => <Product key={product.id}
                     product={product}
+                    handleAddtoCart={handleAddtoCart}
                     ></Product>)
                 }
             </div>
             <div className='cart-container'>
                 <h4>Order summary</h4>
+                <p>Selected Items: {cart.length}</p>
             </div>
         </div>
     );
