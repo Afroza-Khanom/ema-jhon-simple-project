0844d06bdb3ddf5f4519610de5c97b8c8fe3ff5e
Afroza-Khanom <afrozafra.aa@gmail.com>
2023-03-31 13:12:41 +0600

    set value to local storage

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../cart/Cart';
 import Product from '../product/Product';
 import './Shop.css';
@@ -13,10 +14,32 @@ const Shop = () => {
         .then(data => setProducts(data))
     },[])
 
+    useEffect(()=>{
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        //step 1 get id of the addedProduct
+        for(const id in storedCart){
+            //step 2 get the product by using id
+            const addedProduct = products.find(product => product.id === id)
+            if(addedProduct){
+                // step 3 add quantity 
+                const quantity = storedCart[id];
+                addedProduct.quantity= quantity;
+                // step 4 add the added product to saved cart
+                savedCart.push(addedProduct);
+            }
+            // step 5 set the cart
+            setCart(savedCart);
+
+            
+        }
+    },[products])
+
     const handleAddtoCart = (product) =>{
         // cart.push(product) notmally js e evbe cart e item add kra jeto
         const newCart = [...cart, product];  //react e new array ready kore then add krte hoi
         setCart(newCart)
+        addToDb(product.id)
     }
 
     return (
