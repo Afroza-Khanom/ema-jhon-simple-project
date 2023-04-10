e2400388a47f68f7f2a06236cda798f87225f422
Afroza-Khanom <afrozafra.aa@gmail.com>
2023-04-11 04:17:33 +0600

    implement delete cart

---
+++ src/components/orders/Orders.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Cart from '../cart/Cart';
+import { useLoaderData } from 'react-router-dom';
+import ReviewItem from '../reviewItem/ReviewItem';
+import './Orders.css'
+import { removeFromDb } from '../../utilities/fakedb';
+
+const Orders = () => {
+    const savedCart = useLoaderData();
+    const [cart, setCart] = useState(savedCart)
+
+    const handleRemoveFromCart = (id) => {
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
+    // console.log(savedCart.length)
+    return (
+        <div className='shop-container'>
+            <div className='review-container'>
+            {
+                cart.map(product => <ReviewItem key={product.id} product={product}
+                    handleRemoveFromCart={handleRemoveFromCart}></ReviewItem>)
+            }
+
+            </div>
+            <div className='cart-container'>
+                <Cart cart={cart}></Cart>   
+                {/* cart={[]} eta cart er array ta ke loop korbe just empty array thakai empty dekhabe but jdi na di  tahole error dbe */}
+            </div>
+        </div>
+    );
+};
+
+export default Orders;
